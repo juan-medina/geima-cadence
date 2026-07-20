5564f45be82d00dfed3d76e2b9ce015cb0953f02
Juan Medina <jamedina@gmail.com>
2026-07-20 15:46:18 +0100

    better web shell

--- src/game/game.gd
+++ src/game/game.gd
@@ -7,12 +7,22 @@ extends Node2D
 @onready var music: AudioStreamPlayer = $Music
 @onready var _start_overlay: CanvasLayer = $StartOverlay
 @onready var _start_button: Button = $StartOverlay/Start
+@onready var _camera: Camera2D = $Camera2D
 
 
 func _ready() -> void:
 	# Browsers refuse to start audio before a user gesture, so the song only
 	# begins once the player has pressed something.
 	_start_button.pressed.connect(_on_start_pressed)
+
+	# Workaround for Godot 4 Camera2D not re-centering after the window resizes.
+	get_tree().root.size_changed.connect(_on_window_resized)
+
+
+func _on_window_resized() -> void:
+	_camera.enabled = false
+	_camera.enabled = true
+	_camera.force_update_scroll()
 
 
 func _on_start_pressed() -> void:
