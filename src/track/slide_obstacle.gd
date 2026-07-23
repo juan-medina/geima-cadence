c72fd1c6e2fcc4b3df71859e44113d86815b826e
Juan Medina <jamedina@gmail.com>
2026-07-23 12:18:17 +0100

    Add impact animation to slide obstacle

--- src/track/slide_obstacle.gd
+++ src/track/slide_obstacle.gd
@@ -4,6 +4,8 @@
 class_name SlideObstacle
 extends Obstacle
 
+@onready var animated_sprite2d: AnimatedSprite2D = $AnimatedSprite2D
+
 
 func _init() -> void:
 	type = Type.SLIDE
@@ -15,4 +17,7 @@ func _on_player_success() -> void:
 
 func _on_player_failure() -> void:
 	hit_player.emit(damage)
+	if animated_sprite2d:
+		animated_sprite2d.play("impact")
+		await animated_sprite2d.animation_finished
 	queue_free()
