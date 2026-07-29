4bcd7e9c2195993a5ead3084157c21f3e42d41e5
Juan Medina <jamedina@gmail.com>
2026-07-29 10:55:25 +0100

    Add input scrolling to About panel

--- src/about_panel/about_panel.gd
+++ src/about_panel/about_panel.gd
@@ -7,14 +7,25 @@ extends PanelContainer
 signal back_requested
 
 const ABOUT_PATH: String = "res://data/assets/about.txt"
+const SCROLL_SPEED: float = 500.0
+
+var _scroll_bar: VScrollBar
 
 @onready var _back_button: Button = $VBox/Back
-@onready var _about_text: RichTextLabel = $VBox/AboutTextl
+@onready var _about_text: RichTextLabel = $VBox/AboutText
 
 
 func _ready() -> void:
 	Audio.connect_menu_sounds(self)
+	_scroll_bar = _about_text.get_v_scroll_bar()
 	_load_about_text()
+	set_process(false)
+
+
+func _process(delta: float) -> void:
+	var scroll_input: float = Input.get_axis(&"ui_up", &"ui_down")
+	if absf(scroll_input) > 0.1:
+		_scroll_bar.value += scroll_input * SCROLL_SPEED * delta
 
 
 func _load_about_text() -> void:
@@ -35,11 +46,13 @@ func _on_about_text_meta_clicked(meta: Variant) -> void:
 
 func open() -> void:
 	visible = true
+	set_process(true)
 	_back_button.grab_focus()
 
 
 func close() -> void:
 	visible = false
+	set_process(false)
 
 
 func _on_back_pressed() -> void:
