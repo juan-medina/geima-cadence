ebdc2f5cd33dca3af6dcf3d77960d380c2c1b7ae
Juan Medina <jamedina@gmail.com>
2026-07-31 09:50:21 +0100

    Enhance SongSelectionPanel: Implement difficulty selection and update button text

--- src/song_selection_panel/song_selection_panel.gd
+++ src/song_selection_panel/song_selection_panel.gd
@@ -9,6 +9,10 @@ signal back_requested
 @export var catalogue: Catalogue = null
 
 @onready var _play_button: Button = %Play
+@onready var _easy_button: Button = %Easy
+@onready var _normal_button: Button = %Normal
+@onready var _hard_button: Button = %Hard
+
 
 func _ready() -> void:
 	super._ready()
@@ -17,12 +21,25 @@ func _ready() -> void:
 		get_tree().quit()
 		return
 
+
 func first_focus_control() -> Control:
 	return _play_button
+
 
 func _on_back_pressed() -> void:
 	back_requested.emit()
 
 
+func _selected_difficulty() -> Track.DifficultType:
+	match _easy_button.button_group.get_pressed_button():
+		_easy_button:
+			return Track.DifficultType.EASY
+		_normal_button:
+			return Track.DifficultType.NORMAL
+		_hard_button:
+			return Track.DifficultType.HARD
+	return Track.DifficultType.EASY
+
+
 func _on_play_pressed() -> void:
-	await Transition.go_to_game(Track.DifficultType.EASY, 1)
+	await Transition.go_to_game(_selected_difficulty(), 1)
