261a664aa1a97b5b7cc50a3740940e516c38e3ab
Juan Medina <jamedina@gmail.com>
2026-08-02 08:18:36 +0100

    fire explosion sound

--- src/sound/sound.gd
+++ src/sound/sound.gd
@@ -27,7 +27,6 @@ const SLIDE: AudioStream = preload("res://data/assets/sounds/19_Slide_01.wav")
 const SLASH_1: AudioStream = preload("res://data/assets/sounds/31_swoosh_sword_1.wav")
 const SLASH_2: AudioStream = preload("res://data/assets/sounds/32_swoosh_sword_2.wav")
 const SLASH_3: AudioStream = preload("res://data/assets/sounds/33_swoosh_sword_3.wav")
-
 const SLASHES: Array[AudioStream] = [SLASH_1, SLASH_2, SLASH_3]
 
 const HIT_1: AudioStream = preload("res://data/assets/sounds/62_Get_hit_01.wav")
@@ -35,6 +34,10 @@ const HIT_2: AudioStream = preload("res://data/assets/sounds/63_Get_hit_02.wav")
 const HITS: Array[AudioStream] = [HIT_1, HIT_2]
 
 const DIE: AudioStream = preload("res://data/assets/sounds/68_Die_01.wav")
+
+const FIRE_EXPLOSION_1: AudioStream = preload("res://data/assets/sounds/01_Fire_explosion_01_small.wav")
+const FIRE_EXPLOSION_2: AudioStream = preload("res://data/assets/sounds/02_Fire_explosion_02_small.wav")
+const FIRE_EXPLOSIONS: Array[AudioStream] = [FIRE_EXPLOSION_1, FIRE_EXPLOSION_2]
 
 var _step_timer: Timer = null
 
@@ -46,6 +49,7 @@ var _slide_sound: AudioStreamPlayer = null
 var _slash_sound: AudioStreamPlayer = null
 var _hit_sound: AudioStreamPlayer = null
 var _die_sound: AudioStreamPlayer = null
+var _fire_explosion_sound: AudioStreamPlayer = null
 
 
 func _ready() -> void:
@@ -64,6 +68,7 @@ func _ready() -> void:
 	_slash_sound = create_random_sound(SLASHES, 1.1)
 	_hit_sound = create_random_sound(HITS, 1.1)
 	_die_sound = create_sound(DIE)
+	_fire_explosion_sound = create_random_sound(FIRE_EXPLOSIONS, 1.1)
 
 
 func create_random_sound(streams: Array[AudioStream], pitch: float) -> AudioStreamPlayer:
@@ -126,8 +131,13 @@ func play_slash() -> void:
 
 
 func play_hit() -> void:
-	_hit_sound.play()
+	#_hit_sound.play()
+	pass
 
 
 func play_die() -> void:
 	_die_sound.play()
+
+
+func play_fire_explosion() -> void:
+	_fire_explosion_sound.play()
