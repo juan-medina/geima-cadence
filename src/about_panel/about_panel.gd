f5a8862b4865b4e860d1ee63e235c2a22c67ed23
Juan Medina <jamedina@gmail.com>
2026-08-10 07:08:59 +0100

    add website button functionality

--- src/about_panel/about_panel.gd
+++ src/about_panel/about_panel.gd
@@ -5,8 +5,9 @@ class_name AboutPanel
 extends MenuPanel
 
 const ABOUT_PATH: String = "res://data/assets/about.txt"
+const WEBSITE_URL: String = "https://github.com/juan-medina/geima-cadence"
 
-@onready var _back_button: Button = $VBox/Back
+@onready var _back_button: Button = $VBox/Buttons/Back
 @onready var _about_text: BigTextPanel = $VBox/AboutText
 
 
@@ -17,6 +18,10 @@ func _ready() -> void:
 
 func first_focus_control() -> Control:
 	return _back_button
+
+
+func _on_website_pressed() -> void:
+	OS.shell_open(WEBSITE_URL)
 
 
 func _on_back_pressed() -> void:
